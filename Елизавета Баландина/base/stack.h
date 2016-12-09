57bc3f12b40c8d62e332908d1e21915b8439e874
Баландина Елизавета <balandina_liza@mail.ru>
2016-12-09 21:14:34 +0300

    Класс стек

--- Елизавета Баландина/base/stack.h
+++ Елизавета Баландина/base/stack.h
@@ -3,25 +3,72 @@
 
 const int MaxStackSize = 100;
 
-template <class T>
+template <class Type>
 class TStack
 {
-  T *pMem;
+  Type *pMem;
   int size;
-  int top;
+  int count;
 public:
-  TStack(int _size)
-  {
-    size = _size;
-    top = -1;
-    if ((size < 1) || (size > MaxStackSize))
-      throw size;
-    pMem = new T[size];
-  }
-  ~TStack()
-  {
-    delete [] pMem;
-  }
+	TStack(int _size);
+	~TStack();
+	bool IsEmpty();//проверка пустоты
+	bool IsFull(); // проверка полности
+	void Push(Type val); // положить элемент
+	Type Pop(); //взять элемент
+	Type Top(); // Посмотреть элемент
 };
+
+template <class Type>
+TStack<Type>::TStack(int _size)
+{
+		if ((_size<1) || (_size>MaxStackSize)) throw(_size);
+		size = _size;
+		count = 0;
+		pMem = new Type[size];
+}
+
+template<class Type>
+TStack<Type>::~TStack()
+{
+	delete[] pMem;
+}
+
+template<class Type>
+bool TStack<Type>::IsEmpty()
+{
+	return(count == 0);
+}
+
+template<class Type>
+ bool TStack<Type>::IsFull()
+{
+	return (count == size);
+}
+
+template<class Type>
+ void TStack<Type>::Push(Type val)
+{
+	if (IsFull()) throw(val);
+	pMem[count++] = val;
+	
+}
+
+template<class Type>
+ Type TStack<Type>::Pop()
+{
+	if (IsEmpty()) throw("Empty");
+	count--;
+	return pMem[count];
+}
+
+template<class Type>
+Type TStack<Type>::Top()
+{
+	if (IsEmpty()) throw("Empty");
+	return pMem[count-1];
+}
+
+
 
 #endif
